cb1b2dbf0b2267efc504d7a571a5d3040ceb9d51
luckygogreen <luckygogreen@users.noreply.github.com>
2020-02-17 17:30:32 -0500

    restart install service and use mysql

--- AccessGateway/__init__.py
+++ AccessGateway/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, unicode_literals
-
+import pymysql
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
 
 __all__ = ['celery_app']
+pymysql.install_as_MySQLdb()  # 告诉django用pymysql代替mysqldb连接数据库
--- AccessGateway/settings.py
+++ AccessGateway/settings.py
@@ -78,11 +78,24 @@ WSGI_APPLICATION = 'AccessGateway.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+#sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+# mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'accessgateway',
+        'USER': 'accessgateway',
+        'PASSWORD': '7ECgIfMhjNhsIzzX',
+        'HOST': '0.0.0.0',
+        'PORT': '3306',
+        'OPTIONS':{"init_command": "SET foreign_key_checks = 0;",}
     }
 }
 
